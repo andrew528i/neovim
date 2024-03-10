0a33a7446a89668596ebdb32c14d729403e40f10
Андрей Рыбалко <r72-andrey@monowork.pro>
2024-03-10 22:16:01 +0300

    fix(auto-session): ignoring folds in order to solve unpredictable session crashes

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -2,6 +2,7 @@ return {
   "rmagatti/auto-session",
   lazy = false,
   config = function()
+    vim.opt.sessionoptions = "blank,buffers,curdir,help,tabpages,winsize,winpos,terminal,localoptions"
     require("auto-session").setup {
       pre_save_cmds = { "Neotree close" },
       pre_restore_cmds = { "%bd" },
