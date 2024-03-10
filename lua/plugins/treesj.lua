9b8d703f22ed8cc9759bba4a7c27c681adae06f1
Андрей Рыбалко <r72-andrey@monowork.pro>
2024-03-10 22:22:13 +0300

    refact(treesj): ignore default keybindings, using only split toggle keymap

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -2,5 +2,9 @@ return {
   "Wansmer/treesj",
   opts = {
     max_join_length = 180,
+    use_default_keymaps = false,
+  },
+  keys = {
+    { "<leader>s", "<cmd>TSJToggle<cr>", desc = "Toggle split-join" },
   },
 }
