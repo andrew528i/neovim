125a45a6df188246aeda2f86e8b50fd46ea1f1f8
Андрей Рыбалко <r72-andrey@monowork.pro>
2024-03-23 13:57:19 +0300

    fix(none-ls): unneeded formatters and other none's builtings commented

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,23 +8,26 @@ return {
 
     null_ls.setup({
       sources = {
-        null_ls.builtins.formatting.stylua.with({
-          extra_args = {
-            "--indent_type", "Spaces",
-            "--indents_width", "2",
-            "--quote_style", "ForceDouble"
-          },
-        }),
-        null_ls.builtins.completion.spell,
-        null_ls.builtins.completion.tags,
-        -- null_ls.builtins.completion.luasnip,
-        -- null_ls.builtins.completion.vsnip,
-        -- null_ls.builtins.diagnostics.golangci_lint,
-        null_ls.builtins.formatting.gofumpt,
-        null_ls.builtins.formatting.goimports_reviser,
         null_ls.builtins.code_actions.gomodifytags,
-        null_ls.builtins.code_actions.refactoring,
-        null_ls.builtins.formatting.gofmt,
+        null_ls.builtins.code_actions.impl,
+
+        -- -- null_ls.builtins.completion.tags,
+
+        -- null_ls.builtins.diagnostics.buf,
+        -- null_ls.builtins.diagnostics.codespell,
+        -- null_ls.builtins.diagnostics.fish,
+        -- null_ls.builtins.diagnostics.golangci_lint,
+        -- null_ls.builtins.diagnostics.hadolint,
+        -- null_ls.builtins.diagnostics.markdownlint,
+        null_ls.builtins.diagnostics.solhint,
+
+        -- null_ls.builtins.formatting.biome,
+        -- null_ls.builtins.formatting.buf,
+        -- null_ls.builtins.formatting.codespell,
+        -- null_ls.builtins.formatting.gofumpt,
+        -- null_ls.builtins.formatting.goimports_reviser,
+        -- null_ls.builtins.formatting.markdownlint,
+        -- null_ls.builtins.formatting.prettier,
       },
     })
   end,
